3c144ab1c4727293e080d401f249f851d07a321b
mseltahir <mohdsst@gmail.com>
2019-08-13 19:39:27 +0200

    added user registeration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:blog_page')
+    else:
+        form = UserRegisterForm()
+    context = {
+        'title': 'Sign Up',
+        'form': form
+    }
+    return render(request, 'users/register.html', context=context)
