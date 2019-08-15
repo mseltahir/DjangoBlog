494aadf7c9b1d888a70dc646306e011081d2f59d
mseltahir <mohdsst@gmail.com>
2019-08-15 17:16:07 +0200

    sidebar login and overall styling

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,10 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_URL = 'users:login'
-LOGIN_REDIRECT_URL = 'users:profile'
+LOGIN_REDIRECT_URL = 'blog:blog_page'
 
 
 django_heroku.settings(locals())
